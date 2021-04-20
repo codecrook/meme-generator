506716d7a41e32479c7be439839b62a2d9638b8d
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-04-20 22:14:35 +0530

    MOD: MemeGenerator - Fetch all images and save them in state

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -3,7 +3,19 @@ import React, { Component } from 'react'
 export default class MemeGenerator extends Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            allMemeImgs: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                const { memes } = response.data
+                console.log(memes[0])
+                this.setState({ allMemeImgs: memes })
+            });
     }
 
     render() {
